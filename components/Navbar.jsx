a1088dc81bd4a0191263eb0ed35da825b888b6a0
Ahsan Zamil <ahsanzamil76@gmail.com>
2025-08-01 17:49:53 +0530

    Fixed logo position behavior during scrolling

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -31,8 +31,8 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
     </div>
 
       <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20" : ""}`}>
-        <a href="#top" className="absolute top-4 left-5">
-            <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-28 alt="" cursor-pointer'/>
+        <a href="#top" className={`${isScroll ? "" : "absolute top-4 left-5 md:relative md:top-auto md:left-auto"}`}>
+            <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-28 alt="" cursor-pointer mr-14'/>
         </a>
 
         <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"} `}>
